eb7747f06aa9dfb9ecd4dfe255bffc4e73644354
axie66 <alex.xie7734@gmail.com>
2021-02-26 12:27:32 -0500

    Updates

---
+++ precache-manifest.535f49acd748ad308405a1d2aee1b204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7d4890d266dc78b61249f87022cc03",
+    "url": "/fpl-helper/index.html"
+  },
+  {
+    "revision": "67217056e5481e49b9d6",
+    "url": "/fpl-helper/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "31e708b06b4359ea8f4a",
+    "url": "/fpl-helper/static/js/2.1e0c6c49.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fpl-helper/static/js/2.1e0c6c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67217056e5481e49b9d6",
+    "url": "/fpl-helper/static/js/main.11e343e6.chunk.js"
+  },
+  {
+    "revision": "6246b98b69cfb97b61b3",
+    "url": "/fpl-helper/static/js/runtime-main.73f4de37.js"
+  }
+]);
